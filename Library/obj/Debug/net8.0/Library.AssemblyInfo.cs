707dda2f940093fe049c7bcb810b3f1149b791e7
Maria Victoria Riccetto <maria.riccetto@correo.ucu.edu.uy>
2024-10-09 10:02:13 -0300

    para pullear

--- Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3eec4a02a121efa2903b87cd619201b534f26d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a0337dcb50b770176e927fc20f2ee1e5078de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

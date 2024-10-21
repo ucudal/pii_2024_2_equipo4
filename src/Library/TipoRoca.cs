a85e9cbc00fa63d5bad40e4848f04ffd3197cb8c
Seba_Fon <sebastian.fonseca@correo.ucu.edu.uy>
2024-10-21 14:09:08 -0300

    Agregé los comentarios

--- src/Library/TipoRoca.cs
+++ src/Library/TipoRoca.cs
@@ -8,13 +8,14 @@ public class TipoRoca:ITipo
     {
         if (tipoAtaque is TipoAgua || tipoAtaque is TipoLucha || tipoAtaque is TipoPlanta || tipoAtaque is TipoTierra)
         {
-            return 2.0;
+            return 2.0; //Es devil contra estos tipos
         }
 
         if (tipoAtaque is TipoFuego || tipoAtaque is TipoNormal || tipoAtaque is TipoVeneno ||
             tipoAtaque is TipoVolador)
         {
-            return 0.5;
+            return 0.5; // Es resistente contra estos tipos
         }
+        return 1.0;
     }
 }

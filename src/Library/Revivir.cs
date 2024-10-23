22403926b008ffd85cc37561e057d57ec07c6816
Seba_Fon <sebastian.fonseca@correo.ucu.edu.uy>
2024-10-23 10:46:17 -0300

    Cree el item revivir

---
+++ src/Library/Revivir.cs
@@ -0,0 +1,17 @@
+﻿using Library.Interfaces;
+
+namespace Library;
+
+public class Revivir:IItem
+{
+    public string Nombre { get; } = "Revivir";
+
+    public void RevivirPokemon(Pokemon pokemon)
+    {
+        pokemon.HP += pokemon.HPMax / 2;
+        if (pokemon.HP > pokemon.HPMax)
+        {
+            pokemon.HP = pokemon.HPMax;
+        }
+    }
+}
